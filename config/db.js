02b0f9d5a020d834a98f23064a61a62b4d4a841c
sharky412 <sharky412@gmail.com>
2024-11-27 17:05:17 +0530

    third commit

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
+require('dotenv').config();  // Load environment variables from .env file
+
 const mysql = require('mysql2/promise');
 
+// Create MySQL connection pool using environment variables
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '$Hardul412',
-    database: 'school_management'
+  host: process.env.MYSQLHOST,  // MySQL host from environment variable
+  user: process.env.MYSQLUSER,  // MySQL user from environment variable
+  password: process.env.MYSQLPASSWORD,  // MySQL password from environment variable
+  database: process.env.MYSQLDATABASE  // MySQL database from environment variable
 });
 
 module.exports = pool;
